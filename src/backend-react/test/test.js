f4ed50bf99695310e4cc3f6dcb227e903fdb5600
Lucas Silveira <50899678+lucassilveira3@users.noreply.github.com>
2022-12-12 16:20:54 -0300

    Update test.js

--- src/backend-react/test/test.js
+++ src/backend-react/test/test.js
@@ -12,14 +12,36 @@ const queries = require('../server/queries.js')
 //     res.json({ message: "Hello from server!" });
 // });
 
-test('Hello world', () => {
-  const res = app.get('/api');
+test('Users endpoint', () => {
+  const res = app.get('/users');
+  expect(res.status).toEqual(200);
+});
+
+
+test('Products endpoint', () => {
+  const res = app.get('/products');
   //expect(res.status).toEqual(200);
 });
 
 
-// describe('User Endpoints', () => {
+test('Orders endpoint', () => {
+  const res = app.get('/order_requested');
+  //expect(res.status).toEqual(200);
+});
 
+
+test('Stock endpoint', () => {
+  const res = app.get('/stock');
+  //expect(res.status).toEqual(200);
+});
+
+
+test('Hello from server', () => {
+  const res = app.get('/api');
+  //expect(res.status).toEqual(200);
+});
+
+// describe('User Endpoints', () => {
 //     it('GET /users should show all users', async () => {
 //       const res = await requestWithSupertest.get('/users');
 //         expect(res.status).toEqual(200);
