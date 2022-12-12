7a1eb4050d3733215fd6eccefd7bdf2f68e50b35
Lucas Silveira <50899678+lucassilveira3@users.noreply.github.com>
2022-12-12 14:24:31 -0300

    Update test.js

--- src/backend-react/test/test.js
+++ src/backend-react/test/test.js
@@ -1,14 +1,28 @@
+const express = require("express");
+const app = express();
 const server = require('../server/index.js');
-const supertest = require('supertest');
-const requestWithSupertest = supertest(server);
+const queries = require('../server/queries.js')
 
-describe('User Endpoints', () => {
+// test('User Endpoints', () => {
+//     const res = queries.getUsers('/users')
+//     expect()
+// });
 
-    it('GET /users should show all users', async () => {
-      const res = await requestWithSupertest.get('/users');
-        expect(res.status).toEqual(200);
-        expect(res.type).toEqual(expect.stringContaining('json'));
-        expect(res.body).toHaveProperty('users')
-    });
+test('Hello world', () => {
+    expect((3).toBe(3));
+};
+
+// app.get("/api", (req, res) => {
+//     res.json({ message: "Hello from server!" });
+// });
+
+// describe('User Endpoints', () => {
+
+//     it('GET /users should show all users', async () => {
+//       const res = await requestWithSupertest.get('/users');
+//         expect(res.status).toEqual(200);
+//         expect(res.type).toEqual(expect.stringContaining('json'));
+//         expect(res.body).toHaveProperty('users')
+//     });
   
-  });
+//   });
