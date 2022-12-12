55f31ee7955535584853354946a3d76d4647cbb1
Lucas Silveira <50899678+lucassilveira3@users.noreply.github.com>
2022-12-12 20:44:45 -0300

    Update test.js

--- src/backend-react/test/test.js
+++ src/backend-react/test/test.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 const server = require('../server/index.js');
-const queries = require('../server/queries.js')
 
 test('Hello from server', () => {
   const res = app.get('/api');
@@ -10,28 +9,24 @@ test('Hello from server', () => {
 
 test('Users endpoint', () => {
   const res = app.get('/users');
-  const req = queries.getUsers();
   //expect(res.status).toEqual(200);
 });
 
 
 test('Products endpoint', () => {
   const res = app.get('/products');
-  const req = queries.getProducts();
   //expect(res.status).toEqual(200);
 });
 
 
 test('Orders endpoint', () => {
   const res = app.get('/order_requested');
-  const req = queries.getOrders();
   //expect(res.status).toEqual(200);
 });
 
 
 test('Stock endpoint', () => {
   const res = app.get('/stock');
-  const req = queries.getStockProducts();
   //expect(res.status).toEqual(200);
 });
 
